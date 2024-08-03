8144dc9912bc364aed26c021dab00f29e979a6be
Rafael <rafael@git.com>
2024-08-03 17:16:17 -0300

    serializers-tests

--- order/models/order.py
+++ order/models/order.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
- class Order(models.Model):
-     product = models.ManyToManyField(product, blank=False)
-     user = models.ForeignKey(User, on_delete=models.CASCADE) 
+from product.models import Product
+
+
+class Order(models.Model):
+    product = models.ManyToManyField(Product, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+ 
 
