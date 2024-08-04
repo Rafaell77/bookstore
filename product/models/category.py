5d8ba962a9208586daa515f592e68a2f9dc7d57c
Rafael <rafael@git.com>
2024-08-04 20:40:03 -0300

    viewset

--- product/models/category.py
+++ product/models/category.py
@@ -1,12 +1,11 @@
-    from django.db import models
+from django.db import models
 
-    class Product(models.Model):
-        title = models.CharField(max_length=100)
-        slug = models.SlugField(unique=True)
-        description = models.CharField(max_length=200, blank=True, null=True)
-        active = models.BooleanField(default=True)
-        
-    
-        def __unicode__(self):
-            return self.title
-                         title[buffer]
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.CharField(max_length=200, blank=True, null=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
